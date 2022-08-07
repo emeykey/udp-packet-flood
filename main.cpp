232e1ab79409e3322a5546b42bc17adb488083e7
Emey Key <32582673+emeykey@users.noreply.github.com>
2022-08-07 01:49:38 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,14 +9,17 @@
 #include <sys/socket.h> // socket, sendto
 #include <unistd.h> // close
 
+float bandwidth_limit;
+
 int main(int argc, char *argv[])
 {
-    if (argc == 4)
+    if (argc >= 4)
     {
         auto start_time = std::chrono::high_resolution_clock::now(); //Cas spusteni programu
         float traffic_count = 0; //Celkova velikost odeslanych dat
         float bandwidth_count = 0; //To same ale pro vypocet rychlosti
         std::string msg = "";
+        float bandwidth_limit = 0;
         for (int i=0; i < atoi(argv[3]); i++) //Vytvori obsah paketu po uroven definovane velikosti
             msg = msg + "A";
 
@@ -36,8 +39,17 @@ int main(int argc, char *argv[])
             auto current_time = std::chrono::high_resolution_clock::now(); //Aktualni cas
             traffic_count = traffic_count + (float)n_bytes/1024/1024; //Pripocte k celkovemu poctu velikost aktualne odeslaneho paketu
             bandwidth_count = bandwidth_count + (float)n_bytes*8/1024/1024; //To same akorat kvuli pocitani rychlosti
-            std::cout << std::fixed << std::setprecision(2) << "Bandwidth used: " << bandwidth_count/(float)std::chrono::duration_cast<std::chrono::seconds>(current_time - start_time).count() << " Mb/s | " << traffic_count << " MB total traffic sent." << "\r"; //Vyplivne aktualni info
+            float bandwidth = bandwidth_count/(float)std::chrono::duration_cast<std::chrono::seconds>(current_time - start_time).count(); //Rychlost v megabitech za vterinu
+            std::cout << std::fixed << std::setprecision(2) << "Bandwidth used: " << bandwidth << " Mb/s | " << traffic_count << " MB total traffic sent." << "\r"; //Vyplivne aktualni info
             ::close(sock);
+            if (argv[4] != NULL)
+            {
+                bandwidth_limit = atof(argv[4]);
+                if (bandwidth_limit != 0 && bandwidth > bandwidth_limit)
+                {
+                    sleep(1);
+                }
+            }
         }
 
         return 0;
@@ -45,7 +57,7 @@ int main(int argc, char *argv[])
 
     else
     {
-        std::cout << "Usage: ./packetspam <ip> <port> <bytes>" << std::endl << "Example: ./packetspam 192.168.0.1 80 4096" << std::endl;
+        std::cout << "Usage: ./packetspam <ip> <port> <bytes> [speed_in_mbps_default_max]" << std::endl << "Example: ./packetspam 192.168.0.1 80 4096 100" << std::endl;
         return 1;
     }
 }
